0e3360cf7652742897d9fcab77e8bb4a9d3dd22c
Krishna Mohan <krishna@hcdatainc.com>
2015-06-25 15:06:15 -0700

    Add comments

--- go/src/golib/golib.go
+++ go/src/golib/golib.go
@@ -2,6 +2,9 @@ package golib
 
 import "fmt"
 
+// exported interface starts with Capital Letter hence Golib_main instead of
+// golib_main
+
 func Golib_main() {
 	fmt.Println("golib_main called");
 }
