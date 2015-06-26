9af034985754b867ef8a6ab543b896cb89a5f02d
Krishna Mohan <mohankri@gmail.com>
2015-06-25 21:56:21 -0700

    cpp src included

--- go/cppwrap/cppwrap.cc
+++ go/cppwrap/cppwrap.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include "cppsrc.h"
 using namespace std;
 
 void print()
@@ -8,6 +10,11 @@ void print()
 
 extern "C" {
 	void CCppwrap() {
-		print();
+		cppsrc *src = new cppsrc(1);
+		cppsrc *src1 = new cppsrc(1);
+		string	key = "Krishna ";
+		src->set_key(&key, 1);
+		key = "Mohan";
+		src1->set_key(&key, 1);
 	}
 }
